ff45f1487fa1a3af1a5196d9f678c0b314326afc
Divins Mathew <divinsmathew@gmail.com>
2023-11-11 20:17:41 +0530

    Add basic configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "src/Squircle/index.tsx",
+      name: "react-squircles",
+      fileName: "index",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+    },
+  },
+});
